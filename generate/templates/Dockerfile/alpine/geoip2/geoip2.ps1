f8b410788610f50e8e029f95e9a2a7c3da6af521
Leo <leonardjonathanoh@live.com>
2018-10-30 04:05:19 +0800

    cleanup alpine and ubuntu geoip2 variant installations

--- generate/templates/Dockerfile/alpine/geoip2/geoip2.ps1
+++ generate/templates/Dockerfile/alpine/geoip2/geoip2.ps1
@@ -8,19 +8,20 @@ RUN apk update \
         perl-app-cpanminus \
     && apk add --no-cache \
         perl-datetime \
-        perl-netaddr-ip \
-        perl-libwww \
-        perl-lwp-protocol-https \
-        perl-list-someutils \
-        perl-params-validate \
-        perl-namespace-clean \
-        perl-list-allutils \
         perl-path-class \
+        perl-net-ssleay \
+        #perl-libwww \
+        #perl-lwp-protocol-https \
+        #perl-netaddr-ip \
+        #perl-list-someutils \
+        #perl-params-validate \
+        #perl-namespace-clean \
+        #perl-list-allutils \
         #perl-moo \
         #perl-net-ip \
     && cpanm \
-        GeoIP2 \
         MaxMind::DB::Reader \
+        GeoIP2 \
 # Install maxmind DB::Reader::XS (faster than MaxMind::DB::Reader)
 # See: https://github.com/maxmind/libmaxminddb
 # NOTE: Not working atm, tests fail at MAXMIND's Net::Works::Network
@@ -29,7 +30,7 @@ RUN apk update \
     #     libmaxminddb \
     #     libmaxminddb-dev \
     # && cpanm \
-    #     MaxMind::DB::Reader::XS \
+         MaxMind::DB::Reader::XS \
     && apk del build-dependencies \
     && rm -rf /root/.cpan /root/.cpanminus /var/cache/apk/*
 '@
