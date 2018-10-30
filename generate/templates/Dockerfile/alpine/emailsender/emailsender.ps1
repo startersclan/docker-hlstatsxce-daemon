de6a0d7eb75bca59cdb40d5f2f319e37f6c0ffe9
Leo <leonardjonathanoh@live.com>
2018-10-30 11:27:33 +0800

    trigger rebuild of 1.0.3 for emailsender variants

--- generate/templates/Dockerfile/alpine/emailsender/emailsender.ps1
+++ generate/templates/Dockerfile/alpine/emailsender/emailsender.ps1
@@ -1,9 +1,15 @@
 @'
-# Install modules for perl-based email
+# Install modules for perl-based email (SMTPS)
 RUN apk update \
     && apk add --no-cache --virtual build-dependencies \
         build-base \
         perl-app-cpanminus \
+    && apk add --no-cache \
+        perl-net-ssleay \
+    # Email::Sender::Simple requires these dependencies
+    && cpanm \
+        MIME::Base64 \
+        Authen::SASL \
     && cpanm \
         Email::Sender::Simple \
     && apk del build-dependencies \
