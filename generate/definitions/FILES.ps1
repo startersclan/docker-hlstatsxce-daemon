4b3a955c2fdb88bc907307956e0d9deb535e4c38
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2023-03-12 13:40:56 +0000

    Chore (ci): Remove unneeded `.gitlab-ci.yml` template

--- generate/definitions/FILES.ps1
+++ generate/definitions/FILES.ps1
@@ -1,8 +1,6 @@
 # Files' definition
 $FILES = @(
     '.github/workflows/ci-master-pr.yml'
-    # '.github/workflows/ci-release.yml'
     '.github/release-drafter.yml'
-    # '.gitlab-ci.yml'
     'README.md'
 )
